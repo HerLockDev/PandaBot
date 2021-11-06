6fbba4c7694f18b2b7c99b070616ac347112bb98
Talhaisteaqq <85591575+AnossaTG@users.noreply.github.com>
2021-11-06 16:15:06 +0300

    Update _pyrosongs.py

--- GroupMenter/modules/_pyrosongs.py
+++ GroupMenter/modules/_pyrosongs.py
@@ -28,7 +28,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 Finding the song...')
+    m = message.reply('🔎 Şarkıyı bulunuyor...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -47,17 +47,17 @@ def song(client, message):
 
     except Exception as e:
         m.edit(
-            "✖️ Found Nothing. Sorry.\n\nTry another keywork or maybe spell it properly."
+            "✖️ Hiçbir şey bulunamadı. Üzgünüz.\n\nBaşka bir arama deneyin veya doğru şekilde heceleyin."
         )
         print(str(e))
         return
-    m.edit("`Downloading Song... Please wait ⏱`")
+    m.edit("`Şarkı indiriliyor... Lütfen bekleyin ⏱`")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n📤 **By**: @GroupMenterRoBot '
+        rep = f'🎙 **Başlık**: [{title[:35]}]({link})\n🎬 **Kaynak**: YouTube\n⏱️ **Süre**: `{duration}`\n👁‍🗨 **Görüntülemeler**: `{views}`\n📤 **BY**:@MyEmiliaBot'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -65,7 +65,7 @@ def song(client, message):
         message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit('❌ Error')
+        m.edit('❌ HATA')
         print(e)
 
     try:
